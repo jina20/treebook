2599be7378164af6fae9c834d0fcf03e1cc9f346
Gina Lee <ejinah89@hotmail.com>
2013-03-25 19:08:31 -0600

    lskdfjlskfj

--- db/migrate/20130326010141_add_user_id_to_statuses.rb
+++ db/migrate/20130326010141_add_user_id_to_statuses.rb
@@ -1,9 +1,8 @@
-class Add < ActiveRecord::Migration
-  def up
-  end
+class AddUserIdToStatuses < ActiveRecord::Migration
+  def change
   	add_column :statuses, :user_id, :integer
   	add_index :statuses, :user_id
   	remove_column :statuses, :name
-  def down
+ 
   end
 end
